f0996c4750ecf99c3abd24d0430b1626542ce466
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2026-08-01 19:04:21 -0400

    feat: converted Config to a build

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,29 +1,35 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem passing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
 }
 
 struct Config {
     query: String,
-    file_path: String
+    file_path: String,
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
-        Config {
-            query: args[1].clone(),
-            file_path: args[2].clone(),
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments.");
         }
+
+        Ok(Config {query: args[1].clone(), file_path: args[2].clone() })
     }
 }
