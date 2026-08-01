c1afd693dbcf7e755a74c8dff7f5beb3bae0d0d2
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2026-08-01 14:40:39 -0400

    chore: removed stray build artifacts

--- minigrep/target/debug/deps/minigrep-3a8e5c68154ea2af.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Sayeed\Desktop\all-repos\rust-mini-projects\minigrep\target\debug\deps\minigrep-3a8e5c68154ea2af.d: src\main.rs
-
-C:\Users\Sayeed\Desktop\all-repos\rust-mini-projects\minigrep\target\debug\deps\libminigrep-3a8e5c68154ea2af.rmeta: src\main.rs
-
-src\main.rs:
--- minigrep/target/debug/deps/minigrep-602d8c45d2debe8b.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Sayeed\Desktop\all-repos\rust-mini-projects\minigrep\target\debug\deps\minigrep-602d8c45d2debe8b.d: src\main.rs
-
-C:\Users\Sayeed\Desktop\all-repos\rust-mini-projects\minigrep\target\debug\deps\libminigrep-602d8c45d2debe8b.rmeta: src\main.rs
-
-src\main.rs:
